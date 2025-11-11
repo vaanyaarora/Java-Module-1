0d4fcc0ca0c2c4355e2410ddf532d55b5003e249
vaanyaarora <vaanya.ind@gmail.com>
2025-11-12 01:18:27 +0530

    Modify StudentDemo to use Scanner for input
    Updated StudentDemo to take user input for student details.

---
+++ StudentDemo.java
@@ -0,0 +1,50 @@
+class Student {
+    String name;
+    int rollNo;
+
+    void setData(String n, int r) {
+        name = n;
+        rollNo = r;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Roll No: " + rollNo);
+    }
+}
+
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setData("Rahul", 101);
+        s1.display();
+    }
+}
+
+import java.util.Scanner;
+class Student {
+    String name;
+    int rollNo;
+    void setData(String n, int r) {
+        name = n;
+        rollNo = r;
+    }
+    void display() {
+        System.out.println("Name: " + name + ", Roll No: " + rollNo);
+    }
+}
+public class StudentDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter student name: ");
+        String name = sc.nextLine();   
+        System.out.print("Enter roll number: ");
+        int rollNo = sc.nextInt();    
+       
+        Student s1 = new Student();
+        s1.setData(name, rollNo);
+       
+        s1.display();
+        sc.close();
+    }
+}
